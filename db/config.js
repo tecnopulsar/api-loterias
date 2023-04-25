38bd389a305b80c3dbce087b2d6677f7845088a4
tecnopulsar <tecnopulsar@gmail.com>
2023-04-25 14:40:55 -0300

    visible

--- db/config.js
+++ db/config.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 async function connect() {
   try {
-    await mongoose.connect(URI);
+    await mongoose.connect('mongodb+srv://tecnopulsar:BhIrgfhn208YjCdC@cluster0.qdzcu7z.mongodb.net/?retryWrites=true&w=majority');
     console.log("Connected");
   } catch (error) {
     console.log(error);
